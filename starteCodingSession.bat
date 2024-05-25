2855cd88684a95c1609f0fcc21e1df38c063d965
Marcel_Z <113604651+GermanCoastguard@users.noreply.github.com>
2024-05-25 18:51:59 +0200

    Create starteCodingSession.bat

---
+++ starteCodingSession.bat
@@ -0,0 +1,13 @@
+
+// startet alle autohotkeys für die geöffnete session
+
+// coding session = {
+      const hotkeysArray = Array.from(hotkeys.sets.powertoys);
+      // hotkeys muss evtl. manuell vergeben werden ( eingefügt werden )
+      hotKeysArray.forEach((hotkey) {
+      windows.openFile(hotkey)
+      }
+
+
+      console.log("primär ursprünglich von colorPicker , ausgeartet in screensppliter , bildschirmanpassung(=> weitere optimierungs , anwendungsbeispiele für ahk)");
+};
